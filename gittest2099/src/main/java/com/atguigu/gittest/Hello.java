29999b951c96077dbba70ae8261cbb7c47026817
hyang <hyang37@163.com>
2023-11-14 20:45:35 +0800

    update 1

--- gittest2099/src/main/java/com/atguigu/gittest/Hello.java
+++ gittest2099/src/main/java/com/atguigu/gittest/Hello.java
@@ -2,10 +2,11 @@ package com.atguigu.gittest;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("杨航杨航，我是你爹！");
-        System.out.println("杨航杨航，我是你爹！");
-        System.out.println("杨航杨航，我是你爹！");
-        System.out.println("杨航杨航，我是你爹！");
-        System.out.println("杨航杨航，我是你爹！");
+
+        System.out.println("杨航杨航，你是我爹！");
+        System.out.println("杨航杨航，你是我爹！");
+        System.out.println("杨航杨航，你是我爹！");
+        System.out.println("杨航杨航，你是我爹！");
+        System.out.println("杨航杨航，你是我爹！");
     }
 }
